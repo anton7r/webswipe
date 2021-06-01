cb2562845ae5dd786f1f7b7441366bc63a1bb356
anton7r <anton.rosnell@elisanet.fi>
2021-06-01 11:42:12 +0300

    refactored

--- src/index.js
+++ src/index.js
@@ -1,100 +1,68 @@
-//the swipe function
+const webswipe = (elem, whileMoveCallback, callback) => {
 
-
-var webswipe = function(elem, whileMoveCallback, callback){
-
-    var eventMove = {
-        x:0,
-        y:0
-    },
-
-    eventStart = {
-        x:0,
-        y:0
-    },
-    isTouch = window.Touch || false;
+    var eventMove = { x: 0, y: 0 };
+    var start = { x: 0, y: 0 };
+    const isTouch = window.Touch || false;
     var previousMove;
 
     var isDone = false,
-    touched = false;
-    
-    var untilDone = function (){
-
-        if(isDone === true){
-            return;
-        }
-
-        var xTotal = eventStart.x - eventMove.x,
-        yTotal = eventStart.y - eventMove.y,
-        xDistance = Math.abs(xTotal),
-        yDistance = Math.abs(yTotal),
-        dir,
-        moved;
-    
-        if (xDistance > yDistance){ //moved on x-axis
-            if (xTotal > 0){
-                dir = "left";
-            } else {
-                dir = "right";
-            }
-            moved = xDistance;
-    
-        } else { //moved on y-axis
-            if (yTotal > 0){
-                dir = "top";
-            } else {
-                dir = "bottom";
-            }
-            moved = yDistance;
-        }
-
-        if(previousMove !== moved){
-            whileMoveCallback(dir, moved);
-        }
-        
-    };
-    
-    function done(){
         touched = false;
-        isDone = true;
-            var xTotal = eventStart.x - eventMove.x,
-            yTotal = eventStart.y - eventMove.y,
+
+    var untilDone = function () {
+        if (isDone) return;
+
+        var xTotal = start.x - eventMove.x,
+            yTotal = start.y - eventMove.y,
             xDistance = Math.abs(xTotal),
             yDistance = Math.abs(yTotal),
             dir,
             moved;
-        
-            if (xDistance > yDistance){ //moved on x-axis
-                if (xTotal > 0){
-                    dir = "left";
-                } else {
-                    dir = "right";
-                }
-                moved = xDistance;
-        
-            } else { //moved on y-axis
-                if (yTotal > 0){
-                    dir = "top";
-                } else {
-                    dir = "bottom";
-                }
-                moved = yDistance;
-            }
-        
-            callback(dir, moved);
-    
+
+        if (xDistance > yDistance) { //moved on x-axis
+            dir = xTotal > 0 ? "left" : "right";
+            moved = xDistance;
+        } else { //moved on y-axis
+            dir = yTotal > 0 ? "top" : "bottom";
+            moved = yDistance;
+        }
+
+        if (previousMove !== moved) {
+            whileMoveCallback(dir, moved);
+        }
+
+    };
+
+    const done = () => {
+        touched = false;
+        isDone = true;
+        var xTotal = start.x - eventMove.x,
+            yTotal = start.y - eventMove.y,
+            xDistance = Math.abs(xTotal),
+            yDistance = Math.abs(yTotal),
+            dir,
+            moved;
+
+        if (xDistance > yDistance) { //moved on x-axis
+            dir = xTotal > 0 ? "left" : "right";
+            moved = xDistance;
+        } else { //moved on y-axis
+            dir = yTotal > 0 ? "top" : "bottom";
+            moved = yDistance;
+        }
+
+        callback(dir, moved);
+
     }
 
-    elem.addEventListener(isTouch ? "touchstart" : "mousedown", function(ev){
+    elem.addEventListener(isTouch ? "touchstart" : "mousedown", ev => {
         isDone = false;
         touched = true;
-        eventStart.x = isTouch ? ev.touches[0].clientX : ev.clientX;
-        eventStart.y = isTouch ? ev.touches[0].clientY : ev.clientY;
+        start.x = isTouch ? ev.touches[0].clientX : ev.clientX;
+        start.y = isTouch ? ev.touches[0].clientY : ev.clientY;
     });
 
-    elem.addEventListener(isTouch ? "touchmove" : "mousemove", function(ev){
-        
-        if(touched === true){
+    elem.addEventListener(isTouch ? "touchmove" : "mousemove", ev => {
+        if (touched === true) {
             eventMove.x = isTouch ? ev.touches[0].clientX : ev.clientX;
             eventMove.y = isTouch ? ev.touches[0].clientY : ev.clientY;
             untilDone();
